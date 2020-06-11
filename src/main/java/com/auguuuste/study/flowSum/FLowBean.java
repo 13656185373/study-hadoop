89ce0b4d434be7ea4a33f19cf6a5d847f6fbf680
陈腾 <teng.chen@bangdao-tech.com>
2020-06-11 18:05:50 +0800

    初始化

---
+++ src/main/java/com/auguuuste/study/flowSum/FLowBean.java
@@ -0,0 +1,25 @@
+package com.auguuuste.study.flowSum;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * @program: study-hadoop
+ * @description:
+ * @author: chenteng
+ * @create: 2020-06-11 18:01
+ **/
+public class FLowBean implements Writable {
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
+}
---
+++ src/main/java/com/auguuuste/study/wordCount/WordcountReducer.java
@@ -0,0 +1,33 @@
+package com.auguuuste.study.wordCount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * @program: study-hadoop
+ * @description:
+ * @author: chenteng
+ * @create: 2020-06-11 17:41
+ **/
+public class WordcountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    int sum;
+    IntWritable v = new IntWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+
+        // 1 累加求和
+        sum = 0;
+        for (IntWritable count : values) {
+            sum += count.get();
+        }
+
+        // 2 输出
+        v.set(sum);
+        context.write(key,v);
+    }
+}
